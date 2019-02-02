6813ba70342bb2c07d426eba7c8459ddde85576a
Gurmeher Singh Chawla <gurmeher.chawla@gmail.com>
2019-02-02 17:38:06 +0530

    Added 'CourseViewModel'

---
+++ app/src/main/java/com/clockworks/incirkle/ViewModels/CourseViewModel.kt
@@ -0,0 +1,10 @@
+package com.clockworks.incirkle.ViewModels
+
+import android.widget.TextView
+
+class CourseViewModel
+{
+    lateinit var codeTextView: TextView
+    lateinit var nameTextView: TextView
+    lateinit var lastestUpdateTextView: TextView
+}
