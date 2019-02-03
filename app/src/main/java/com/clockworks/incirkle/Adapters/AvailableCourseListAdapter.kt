8b71f929b4418d94447c6449003c485ff948811c
Gurmeher Singh Chawla <gurmeher.chawla@gmail.com>
2019-02-03 20:15:18 +0530

    Added AvailableCourseListAdapter

---
+++ app/src/main/java/com/clockworks/incirkle/Adapters/AvailableCourseListAdapter.kt
@@ -0,0 +1,62 @@
+package com.clockworks.incirkle.Adapters
+
+import android.content.Context
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.BaseAdapter
+import android.widget.TextView
+import com.clockworks.incirkle.Models.Course
+import com.clockworks.incirkle.R
+
+class AvailableCourseListAdapter(private val context: Context, private var dataSource: ArrayList<Course>) : BaseAdapter()
+{
+    private class ViewModel
+    {
+        lateinit var courseCode: TextView
+        lateinit var courseName: TextView
+    }
+    private val inflater: LayoutInflater
+            = context.getSystemService(Context.LAYOUT_INFLATER_SERVICE) as LayoutInflater
+
+    override fun getCount(): Int
+    {
+        return this.dataSource.size
+    }
+
+    override fun getItem(p0: Int): Any
+    {
+        return this.dataSource[p0]
+    }
+
+    override fun getItemId(p0: Int): Long
+    {
+        return p0.toLong()
+    }
+
+    override fun getView(position: Int, convertView: View?, parent: ViewGroup?): View
+    {
+        val view: View
+        val viewModel: ViewModel
+
+        if (convertView == null)
+        {
+            view = inflater.inflate(R.layout.list_item_available_course, parent, false)
+            viewModel = ViewModel()
+            viewModel.courseCode = view.findViewById<TextView>(R.id.textView_courseCode)
+            viewModel.courseName = view.findViewById<TextView>(R.id.textView_courseName)
+            view.tag = viewModel
+        }
+        else
+        {
+            view = convertView
+            viewModel = convertView.tag as ViewModel
+        }
+
+        val course = this.getItem(position) as Course
+        viewModel.courseCode.setText(course.code)
+        viewModel.courseName.setText(course.name)
+
+        return view
+    }
+}
