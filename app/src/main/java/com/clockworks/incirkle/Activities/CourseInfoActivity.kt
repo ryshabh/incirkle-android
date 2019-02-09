87feb8f9f50767e3eabec2e269bfffebaf1922bc
Gurmeher Singh Chawla <gurmeher.chawla@gmail.com>
2019-02-09 18:50:51 +0530

    Made the following changes to 'CourseInfoActivity': 1. Setting title of Finish button as 'Enrol Course' if course is not enabled 2. Display of Course Password Alert whilst enrolling course

--- app/src/main/java/com/clockworks/incirkle/Activities/CourseInfoActivity.kt
+++ app/src/main/java/com/clockworks/incirkle/Activities/CourseInfoActivity.kt
@@ -4,6 +4,11 @@ import android.app.Activity
 import android.content.Intent
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.support.v7.app.AlertDialog
+import android.text.Editable
+import android.text.InputFilter
+import android.text.InputType
+import android.text.TextWatcher
 import android.view.View
 import android.widget.*
 import com.clockworks.incirkle.Models.Course
@@ -31,6 +36,7 @@ class CourseInfoActivity : AppCompatActivity()
     private var courseID: String? = null
     private lateinit var course: Course
     private var privilege = PRIVILEGE.NONE
+    private var isCourseEnrolled = false
 
     private fun updateCourse(course: Course, user: User)
     {
@@ -76,6 +82,10 @@ class CourseInfoActivity : AppCompatActivity()
                         task.exception?.let { Toast.makeText(this, it.toString(), Toast.LENGTH_LONG).show() }
                         ?:task.result?.let()
                         {
+                            // Update Button
+                            this.isCourseEnrolled = user.courses.contains(it.reference)
+                            button_finish.setText(if (this.isCourseEnrolled) R.string.text_finish else R.string.title_activity_enrolCourse)
+
                             val course = it.toObject(Course::class.java)!!
                             course.reference = it.reference
                             course.teacher.get().addOnCompleteListener()
@@ -142,6 +152,91 @@ class CourseInfoActivity : AppCompatActivity()
                         }
                 }
         }
+    }
+
+    private fun saveCourse()
+    {
+        val courseReference = this.courseID?.let() { this.coursesCollectionReference.document(it) }
+            ?: run()
+            { this.coursesCollectionReference.document() }
+        courseReference.set(this.course).addOnCompleteListener()
+        {
+                task ->
+
+            task.exception?.let { Toast.makeText(this, it.toString(), Toast.LENGTH_LONG).show() }
+                ?: run()
+            {
+                FirebaseAuth.getInstance().currentUser?.currentUserData()
+                {
+                        user, exception ->
+                    exception?.let { Toast.makeText(this, it.toString(), Toast.LENGTH_LONG).show() }
+                    ?: user?.let()
+                    {
+                        if (this.courseID == null)
+                            it.courses.add(courseReference)
+                        this.saveUser(it, courseReference)
+                    }
+                }
+            }
+        }
+    }
+
+    private fun showEnrolmentAlert()
+    {
+        val coursePasswordEditText = EditText(this)
+        coursePasswordEditText.inputType = InputType.TYPE_CLASS_NUMBER
+        coursePasswordEditText.filters = arrayOf(InputFilter.LengthFilter(4))
+        coursePasswordEditText.setPadding(10, 10, 10, 10)
+
+        val alertBuilder = AlertDialog.Builder(this)
+        alertBuilder.setTitle("Enrol ${this.course.name}")
+        alertBuilder.setMessage("Enter Course Password")
+        alertBuilder.setView(coursePasswordEditText)
+        alertBuilder.setPositiveButton("Enrol", null)
+        alertBuilder.setNeutralButton("Cancel", null)
+
+        val alert = alertBuilder.create()
+
+        coursePasswordEditText.addTextChangedListener(object : TextWatcher
+        {
+            override fun afterTextChanged(p0: Editable?)
+            {
+                alert.getButton(android.support.v7.app.AlertDialog.BUTTON_POSITIVE).isEnabled = p0?.length == 4
+            }
+            override fun beforeTextChanged(p0: CharSequence?, p1: Int, p2: Int, p3: Int)
+            {
+            }
+
+            override fun onTextChanged(p0: CharSequence?, p1: Int, p2: Int, p3: Int)
+            {
+            }
+        })
+
+        alert.setOnShowListener()
+        {
+            dialogInterface ->
+            alert.getButton(android.support.v7.app.AlertDialog.BUTTON_POSITIVE).isEnabled = false
+            (dialogInterface as android.support.v7.app.AlertDialog).getButton(android.support.v7.app.AlertDialog.BUTTON_POSITIVE).setOnClickListener()
+            {
+                button ->
+
+                coursePasswordEditText.error = null
+                val courseIDString = coursePasswordEditText.text.toString().trim()
+                courseIDString.toIntOrNull()?.let()
+                {
+                    courseID ->
+                    if (courseIDString.equals(course.password, false))
+                    {
+                        saveCourse()
+                        dialogInterface.dismiss()
+                    }
+                    else
+                        coursePasswordEditText.error = "Incorrect Course Password"
+                }
+                ?: run() { coursePasswordEditText.error = "Course Password must be numeric" }
+            }
+        }
+        alert.show()
     }
 
     override fun onCreate(savedInstanceState: Bundle?)
@@ -217,29 +312,10 @@ class CourseInfoActivity : AppCompatActivity()
             this.course.code = courseCode
             this.course.name = courseName
 
-            val courseReference = this.courseID?.let() { this.coursesCollectionReference.document(it) }
-            ?: run()
-            { this.coursesCollectionReference.document() }
-            courseReference.set(this.course).addOnCompleteListener()
-            {
-                task ->
-
-                task.exception?.let { Toast.makeText(this, it.toString(), Toast.LENGTH_LONG).show() }
-                ?: run()
-                {
-                    FirebaseAuth.getInstance().currentUser?.currentUserData()
-                    {
-                        user, exception ->
-                        exception?.let { Toast.makeText(this, it.toString(), Toast.LENGTH_LONG).show() }
-                        ?: user?.let()
-                        {
-                            if (this.courseID == null)
-                                it.courses.add(courseReference)
-                            this.saveUser(it, courseReference)
-                        }
-                    }
-                }
-            }
+            if (this.isCourseEnrolled)
+                saveCourse()
+            else
+                showEnrolmentAlert()
         }
     }
 }
