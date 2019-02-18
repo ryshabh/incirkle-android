e191845d36f3638d293de99f54dac6c2c3e27a26
Gurmeher Singh Chawla <gurmeher.chawla@gmail.com>
2019-02-18 23:04:11 +0530

    Added 'ForumPost' to 'Models'

---
+++ app/src/main/java/com/clockworks/incirkle/Models/ForumPost.kt
@@ -0,0 +1,40 @@
+package com.clockworks.incirkle.Models
+
+import com.google.firebase.Timestamp
+import com.google.firebase.firestore.DocumentReference
+import java.util.*
+import kotlin.collections.ArrayList
+
+class ForumPost()
+{
+    var reference: DocumentReference? = null
+
+    lateinit var name: String
+    lateinit var description: String
+    // TODO: - Attachment
+    lateinit var poster: DocumentReference
+    lateinit var timestamp: Timestamp
+    var comments = ArrayList<Comment>()
+
+    constructor(name: String, description: String, poster: DocumentReference) : this()
+    {
+        this.name = name
+        this.description = description
+        this.poster = poster
+        this.timestamp = Timestamp(Date())
+    }
+
+    class Comment()
+    {
+        lateinit var content: String
+        lateinit var poster: DocumentReference
+        lateinit var timestamp: Timestamp
+
+        constructor(content: String, poster: DocumentReference) : this()
+        {
+            this.content = content
+            this.poster = poster
+            this.timestamp = Timestamp(Date())
+        }
+    }
+}
