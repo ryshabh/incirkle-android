08bb9d7a4ffdf0aa46590d33e1bb6f08e1d27152
Gurmeher Singh Chawla <gurmeher.chawla@gmail.com>
2019-02-26 18:36:57 +0530

    Added 'AssignmentPost'

---
+++ app/src/main/java/com/clockworks/incirkle/Models/AssignmentPost.kt
@@ -0,0 +1,28 @@
+package com.clockworks.incirkle.Models
+
+import com.clockworks.incirkle.Interfaces.FirebaseDocument
+import com.google.firebase.Timestamp
+import com.google.firebase.firestore.DocumentReference
+import java.util.*
+
+class AssignmentPost(): FirebaseDocument
+{
+    override var reference: DocumentReference? = null
+
+    lateinit var name: String
+    lateinit var details: String
+    lateinit var dueDate: Timestamp
+
+    // TODO: - Attachment
+    lateinit var poster: DocumentReference
+    lateinit var timestamp: Timestamp
+
+    constructor(name: String, details: String, dueDate: Date, poster: DocumentReference) : this()
+    {
+        this.name = name
+        this.details = details
+        this.dueDate = Timestamp(dueDate)
+        this.poster = poster
+        this.timestamp = Timestamp(Date())
+    }
+}
