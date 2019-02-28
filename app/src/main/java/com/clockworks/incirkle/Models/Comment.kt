7992c65090a906df0c7657e13ca06946b15c7b40
Gurmeher Singh Chawla <gurmeher.chawla@gmail.com>
2019-02-28 22:05:16 +0530

    Added 'Comment' Model class

---
+++ app/src/main/java/com/clockworks/incirkle/Models/Comment.kt
@@ -0,0 +1,22 @@
+package com.clockworks.incirkle.Models
+
+import com.clockworks.incirkle.Interfaces.FirebaseDocument
+import com.google.firebase.Timestamp
+import com.google.firebase.firestore.DocumentReference
+import java.util.*
+
+class Comment(): FirebaseDocument
+{
+    override var reference: DocumentReference? = null
+
+    lateinit var content: String
+    lateinit var poster: DocumentReference
+    lateinit var timestamp: Timestamp
+
+    constructor(content: String, poster: DocumentReference) : this()
+    {
+        this.content = content
+        this.poster = poster
+        this.timestamp = Timestamp(Date())
+    }
+}
